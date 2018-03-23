c361594e3cd81efcafb958699cfe94cfcdae395a
rhaugerud <rhaugerud@usgs.gov>
2018-03-23 09:37:47 -0700

    set debug = False

--- Scripts/GeMS_SetSymbols_Arc10.py
+++ Scripts/GeMS_SetSymbols_Arc10.py
@@ -15,7 +15,7 @@ unrecognizedTypes = []
 
 dictionaryFile = os.path.dirname(sys.argv[0])+'/../Resources/Type-FgdcSymbol.txt'
 
-debug1 = True
+debug1 = False
 
 def buildSymbolDicts(dFile):
     df = open(dictionaryFile,'r')
