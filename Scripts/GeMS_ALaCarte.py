59b480064cd845a08977c9ad55bd3820336e9d3b
Evan Thoms <ethoms@usgs.gov>
2023-02-07 12:18:47 -0900

    saving work

--- Scripts/GeMS_ALaCarte.py
+++ Scripts/GeMS_ALaCarte.py
@@ -17,6 +17,55 @@ import arcpy
 import GeMS_Definition as gdef
 from pathlib import Path
 
+gdb_dict = {
+    "CMULines": "line",
+    "CMUMapUnitPolys": "polygon",
+    "CMUPoints": "point",
+    "CartographicLines": "line",
+    "ContactsAndFaults": "line",
+    "DataSourcePolys": "polygon",
+    "DataSources": "table",
+    "DescriptionOfMapUnits": "table",
+    "FossilPoints": "point",
+    "GenericPoints": "point",
+    "GenericSamples": "point",
+    "GeoMaterialDict": "table",
+    "GeochronPoints": "point",
+    "GeologicLines": "line",
+    "Glossary": "table",
+    "IsoValueLines": "line",
+    "LayerList": "table",
+    "MapUnitLines": "line",
+    "MapUnitOverlayPolys": "polygon",
+    "MapUnitPoints": "point",
+    "MapUnitPolys": "polygon",
+    "MiscellaneousMapInformation": "table",
+    "OrientationPoints": "point",
+    "OverlayPolys": "polygon",
+    "PhotoPoints": "point",
+    "RepurposedSymbols": "table",
+    "StandardLithology": "table",
+    "Stations": "point",
+}
+
+
+def eval_prj(prj_str, fd):
+    unk_fds = [
+        "CrossSection",
+        "CorrelationOfMapUnits",
+    ]
+    if any([x in fd for x in unk_fds]):
+        # create 'UNKNOWN' spatial reference
+        sr_empty = arcpy.FromWKT("POINT EMPTY").spatialReference
+        sr_str = sr_empty.exportToString()
+        sr = arcpy.SpatialReference()
+        sr.loadFromString(sr_str)
+    else:
+        sr = arcpy.SpatialReference()
+        sr.loadFromString(prj_str)
+
+    return sr
+
 
 def process(gdb, value_table):
     # if gdb doesn't exist, make it
@@ -25,18 +74,20 @@ def process(gdb, value_table):
         name = Path(gdb).stem
         arcpy.CreateFileGDB_management(folder, name)
 
-    arcpy.AddMessage(value_table)
+    out_path = gdb
 
     # collect the values from the valuetable
     for i in range(0, value_table.rowCount):
         fd = value_table.getValue(i, 0)
         sr_prj = value_table.getValue(i, 1)
+        sr = eval_prj(sr_prj, fd)
         fc = value_table.getValue(i, 2)
 
-        # if not fd in ['', ' ', '#', None:
-
-        sr = arcpy.SpatialReference()
-        sr.loadFromString(value_table.getValue(i, 1))
+        if not fd == "#":
+            fd_path = Path(gdb) / fd
+            if not arcpy.Exists(str(fd_path)):
+                arcpy.CreateFeatureDataset_management(gdb, fd, sr)
+                out_path = str(fd_path)
 
 
 if __name__ == "__main__":
