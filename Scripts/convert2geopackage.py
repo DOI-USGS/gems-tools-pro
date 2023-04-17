09d0b5062942f6dca512bf42194adfe04529cf68
Evan Thoms <ethoms@usgs.gov>
2023-04-17 13:18:56 -0800

    working with test gdb

--- Scripts/convert2geopackage.py
+++ Scripts/convert2geopackage.py
@@ -7,62 +7,70 @@ the original name but feature classes inside feature datasets have the
 name of the feature dataset pre-prended to the name of the feature class
 
 Usage: 
-    Provide the path to a .gdb and an optional output folder
+    Provide the path to a .gdb, an optional output folder. If no output 
+    directory is specified, the gpkg will be created in the parent folder
+    of the input gdb. The resulting geopackage will have the same name 
+    as the gdb with a .gpkg extension. Existing geopackages will be deleted 
+    first and then recreated. The parameter form will warn the user about 
+    an existing geopackage. They may proceed or pick a different output folder.
 
 Args:
     input_gdb (str) : Path to database. Required.
-    output_dir (str) : Path to folder in which to build the geopackage. Optional
-    overwrite (boolean) : Overwrite (delete and create new) existing geopackage.
-      True or False. Optional. True by default.
+    output_dir (str) : Path to folder in which to build the geopackage. Optional.
 """
 
-import arcpy, os
+import arcpy
+from pathlib import Path
+from GeMS_utilityFunctions import addMsgAndPrint as ap
+import GeMS_utilityFunctions as guf
 
-def convert(input_gdb, output_dir, overwrite)
+versionString = "GeMS_ALaCarte.py, version of 17 April 2023"
+rawurl = "https://raw.githubusercontent.com/DOI-USGS/gems-tools-pro/master/Scripts/convert2gpkg.py"
+guf.checkVersion(versionString, rawurl, "gems-tools-pro")
+
+
+def convert(input_gdb, output_dir):
     # Set up input and output paths
-    input_gdb = r"path\to\input\geodatabase.gdb"
+    input_gdb = Path(input_gdb)
     if output_dir in (None, "", "#"):
-        output_dir = os.path.dirname(input_gdb)
+        output_dir = input_gdb.parent
 
-    output_gpkg = os.path.join(
-        output_dir, f"{os.path.basename(input_gdb)}.gpkg"
-    )
+    output_gpkg = output_dir / f"{input_gdb.stem}.gpkg"
 
-    if arcpy.Exists(output_gpkg):
-        if overwrite.lower in ("true"):
-            arcpy.Delete_management(output_gpkg)
-        else:
-            
-    
+    if output_gpkg.exists():
+        arcpy.Delete_management(str(output_gpkg))
 
-    # Convert feature classes in feature datasets
-    arcpy.env.workspace = input_gdb
+    ap(f"Creating {input_gdb.stem}.gpkg")
+    arcpy.CreateSQLiteDatabase_management(str(output_gpkg), "GEOPACKAGE_1.3")
+
+    # Export feature classes in feature datasets
+    arcpy.env.workspace = str(input_gdb)
     datasets = arcpy.ListDatasets()
     for dataset in datasets:
         fc_list = arcpy.ListFeatureClasses("", "", dataset)
         for fc in fc_list:
             fc_name = f"{dataset}_{fc}"
-            arcpy.FeatureClassToGeodatabase_conversion(
-                f"{dataset}/{fc}", output_gpkg, fc_name
+            ap(f"Exporting {fc} as {fc_name}")
+            arcpy.ExportFeatures_conversion(
+                f"{dataset}/{fc}", str(output_gpkg / fc_name)
             )
 
-    # Convert tables and feature classes outside feature datasets
-    arcpy.env.workspace = input_gdb
+    # Export tables and feature classes outside feature datasets
+    arcpy.env.workspace = str(input_gdb)
     fc_list = arcpy.ListFeatureClasses()
     table_list = arcpy.ListTables()
     for fc in fc_list:
-        if arcpy.Describe(fc).datasetType != "FeatureDataset":
-            arcpy.FeatureClassToGeodatabase_conversion(fc, output_gpkg, fc)
+        ap(f"Exporting {fc}")
+        arcpy.ExportFeatures_conversion(fc, str(output_gpkg / fc))
     for table in table_list:
-        arcpy.TableToGeodatabase_conversion(table, output_gpkg)
+        ap(f"Exporting {table}")
+        arcpy.ExportTable_conversion(table, str(output_gpkg / table))
 
-    print("Conversion complete.")
+   ap("Export complete.")
 
 
 if __name__ == "__main__":
-
     param0 = arcpy.GetParameterAsText(0)
     param1 = arcpy.GetParameterAsText(1)
-    param2 = arcpy.GetParameterAsText(2)
 
-    convert(param0, param1, param2)
+    convert(param0, param1)
