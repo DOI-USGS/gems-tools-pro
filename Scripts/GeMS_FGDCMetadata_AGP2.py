bf55577eac2eb76112e7becd2da622cd72e681ad
Evan Thoms <ethoms@usgs.gov>
2023-06-13 11:25:51 -0800

    custom attribute dictionary uses lists

--- Scripts/GeMS_FGDCMetadata_AGP2.py
+++ Scripts/GeMS_FGDCMetadata_AGP2.py
@@ -42,6 +42,7 @@ esri_attribs = {
     "override": "BLOB field that stores feature-specific overrides to the cartographic representation rules.",
 }
 
+
 ###### FUNCTIONS
 def gdb_object_dict(gdb_path):
     """Returns a dictionary of table_name: da.Describe_table_properties
@@ -363,7 +364,7 @@ def add_attributes(fc_name, detailed_node):
             found_attrib = True
             del res
 
-        # look for a key ending in the field name in myAttribDict
+        # look for a key myAttribDict
         try:
             res = [key for key in myDef.myAttribDict if field.endswith(key)]
         except:
--- Scripts/my_GeMSDefinitions.py
+++ Scripts/my_GeMSDefinitions.py
@@ -21,39 +21,72 @@
 from GeMS_Definition import *
 
 myAttribDict = {
-    'SamplePretreatment':'Procedure(s) by which some samples were pretreated, before carbon extraction, to minimize the effects of later contamination.',
-    'AnalysisMethod':'How carbon isotopes were analyzed. Not reported for all samples. AMS = accelerator mass spectrometry.',
-    'SampleElevationFt':'Elevation at which sample was collected, in feet above datum.',
-    'delta_13C':'Observed 13C : 12C ratio, reported as an offset, in parts per thousand, relative to standard value PDB. Not reported for all samples.',
-    'CalibratedAge':'2-sigma age range determined from CALIB5.2 program (Stuiver and Reimer, 1993). Rounded to nearest decade. Calculated from reservoir-corrected age for marine samples. Also known as calendrical age.',
-    'Location':'Brief text description of locale at which sample was collected.',
-    'MaterialDated':'Type of material dated. Values include marine shell, peat, detrital wood, etc.',
-    'Lab_no':'Laboratory identifier for analyzed sample.',
-    'ConventionalAge':'Age reported by laboratory. Reflects assumed constant 14C production rate and accepted 14C decay constant. Ages normalized to delta 13C = -25.0 per mil, +/- 1 sigma, lab error multiplier = 1.',
-    'ReservoirCorrectedAge':'The reservoir corrected age for marine shells is the conventional 14C age minus a reservoir value of 1,100 yrs (delta R of 700 +/- 100 yrs; Kovanen and Easterbrook, 2002b). The +/- is the reported laboratory precision and does not include any error associated with determination of the reservoir value. N.A. = not applicable (terrestrial samples). ',
-    'Significance':'Brief discussion of significance of reported age.',
-    'StratigraphicContext':'Stratigraphic horizon from which sample was collected.',
-    'Phase':'Integer index number of time period within late Pleistocene history of map area. Nine phases are recognized.'
-    }
+    "SamplePretreatment": [
+        "Procedure(s) by which some samples were pretreated, before carbon extraction, to minimize the effects of later contamination.",
+        "foobar",
+    ],
+    "AnalysisMethod": [
+        "How carbon isotopes were analyzed. Not reported for all samples. AMS = accelerator mass spectrometry.",
+        "foobar",
+    ],
+    "SampleElevationFt": [
+        "Elevation at which sample was collected, in feet above datum.",
+        "foobar",
+    ],
+    "delta_13C": [
+        "Observed 13C : 12C ratio, reported as an offset, in parts per thousand, relative to standard value PDB. Not reported for all samples.",
+        "foobar",
+    ],
+    "CalibratedAge": [
+        "2-sigma age range determined from CALIB5.2 program (Stuiver and Reimer, 1993). Rounded to nearest decade. Calculated from reservoir-corrected age for marine samples. Also known as calendrical age.",
+        "foobar",
+    ],
+    "Location": [
+        "Brief text description of locale at which sample was collected.",
+        "foobar",
+    ],
+    "MaterialDated": [
+        "Type of material dated. Values include marine shell, peat, detrital wood, etc.",
+        "foobar",
+    ],
+    "Lab_no": ["Laboratory identifier for analyzed sample.", "foobar"],
+    "ConventionalAge": [
+        "Age reported by laboratory. Reflects assumed constant 14C production rate and accepted 14C decay constant. Ages normalized to delta 13C = -25.0 per mil, +/- 1 sigma, lab error multiplier = 1.",
+        "foobar",
+    ],
+    "ReservoirCorrectedAge": [
+        "The reservoir corrected age for marine shells is the conventional 14C age minus a reservoir value of 1,100 yrs (delta R of 700 +/- 100 yrs; Kovanen and Easterbrook, 2002b). The +/- is the reported laboratory precision and does not include any error associated with determination of the reservoir value. N.A. = not applicable (terrestrial samples). ",
+        "foobar",
+    ],
+    "Significance": ["Brief discussion of significance of reported age.", "foobar"],
+    "StratigraphicContext": [
+        "Stratigraphic horizon from which sample was collected.",
+        "foobar",
+    ],
+    "Phase": [
+        "Integer index number of time period within late Pleistocene history of map area. Nine phases are recognized."
+        "foobar"
+    ],
+}
 
 myEntityDict = {
-    'C14Points':'Table that documents locales, sample and analytic information, and significance of selected radiocarbon ages from map area.'
-    }
+    "C14Points": "Table that documents locales, sample and analytic information, and significance of selected radiocarbon ages from map area."
+}
 
 myUnrepresentableDomainDict = {
-    'SamplePretreatment':'Arbitrary string',
-    'AnalysisMethod':'Arbitrary string',
-    'ElevationFt':'Real number',
-    'delta_13C': 'Real number',
-    'CalibratedAge':'Arbitrary string',
-    'Location':'Arbitrary string',
-    'MaterialDated':'Arbitrary string',
-    'Lab_no':'Arbitrary string',
-    'ConventionalAge':'Arbitrary string',
-    'ReservoirCorrectedAge':'Arbitrary string',
-    'Significance':'Arbitrary string',
-    'StratigraphicContext':'Arbitrary string'    
-    }
+    "SamplePretreatment": "Arbitrary string",
+    "AnalysisMethod": "Arbitrary string",
+    "ElevationFt": "Real number",
+    "delta_13C": "Real number",
+    "CalibratedAge": "Arbitrary string",
+    "Location": "Arbitrary string",
+    "MaterialDated": "Arbitrary string",
+    "Lab_no": "Arbitrary string",
+    "ConventionalAge": "Arbitrary string",
+    "ReservoirCorrectedAge": "Arbitrary string",
+    "Significance": "Arbitrary string",
+    "StratigraphicContext": "Arbitrary string",
+}
 
 myEnumeratedValueDomainFieldList = []
 
@@ -65,5 +98,3 @@ def addDefs():
     for f in myEnumeratedValueDomainFieldList:
         enumeratedValueDomainFieldList.append(f)
     return
-
-    
